a7cad9e0ef265976f848b40b9d60ab56e84535b1
miguel.molina <233371@ids.upchiapas.edu.mx>
2025-06-26 19:38:07 -0600

    [TEST] tailwind integration

--- Frontend/voltio_app/src/app/app.component.ts
+++ Frontend/voltio_app/src/app/app.component.ts
@@ -3,8 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'voltio_app';
+  holdInterval: any;
+
+  counter: number = 0;
+
+  increment() {
+    this.counter++;
+  }
+
+  decrement() {
+    if (this.counter > 0) this.counter--;
+  }
+
+  startHolding(action: 'increment' | 'decrement') {
+    this.holdInterval = setInterval(() => {
+      if (action === 'increment') {
+        this.increment();
+      } else {
+        this.decrement();
+      }
+    }, 100);
+  }
+
+  stopHolding() {
+    clearInterval(this.holdInterval);
+  }
+
+  mouseUp() {
+    this.stopHolding();
+  }
 }
